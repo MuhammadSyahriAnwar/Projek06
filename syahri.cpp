4bf674e5071cf26240ff88beb15882904b2b65ff
MuhammadSyahriAnwar <ahmad89.aa70@gmail.com>
2024-07-04 14:48:01 +0700

    add: pecinta binatang

--- syahri.cpp
+++ syahri.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    std::cout << "im ironman" << std::endl;
+    std::cout << "Hello, World" << std::endl;
+    std::cout << "im ironman, pecinta binatang" << std::endl;
     return 0;
 }
